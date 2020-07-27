6b6452de71fc3d0bbf253365d49d02bcca72311f
jarvainen <jarvainen@gmail.com>
2020-07-27 20:22:02 +0300

    Order changes

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ mkdir Skriptit
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo ubuntu-drivers autoinstall
 sudo apt install telegram-desktop
+sudo apt install zsh
+chsh -s $(which zsh)
 sudo apt-get install git
 sudo apt-get install cargo
 sudo apt-get install apt install gcc pkg-config openssl libasound2-dev cmake build-essential python3 libfreetype6-dev libexpat1-dev libxcb-composite0-dev libssl-dev libx11-dev
@@ -10,7 +12,5 @@ sudo apt install gcc pkg-config openssl libasound2-dev cmake build-essential pyt
 chmod +x install-alacritty.sh
 ./install-alacritty.sh
 cd /etc/alternatives/ && sudo ln -sfn $HOME/.cargo/bin/alacritty x-terminal-emulator
-sudo apt install zsh
 cd Lataukset/ && wget https://go.microsoft.com/fwlink/?LinkID=760868 && mv index.html\?LinkID\=760868 vscode.deb && sudo apt install ./vscode.deb
-chsh -s $(which zsh)
 reboot
